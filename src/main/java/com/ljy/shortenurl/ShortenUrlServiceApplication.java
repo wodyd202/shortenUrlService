3a0206adba9c4756d92631e3b95a15e2c7155e7d
loty <jylee04@trenbe.com>
2022-06-19 15:02:04 +0900

    프로젝트 세팅

---
+++ src/main/java/com/ljy/shortenurl/ShortenUrlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ljy.shortenurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenUrlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenUrlServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljy/shortenurl/ShortenUrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljy.shortenurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenUrlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
